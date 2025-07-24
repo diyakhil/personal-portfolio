cbaee4681957e34a57c6aad0b6ba26c854fbfc54
Diya Khilnani <diyakhilnani@Diyas-MacBook-Pro.local>
2025-07-23 22:55:22 -0400

    fixed menu

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -16,24 +16,24 @@ type NavBarProps = {
 export const NavBar = ({ headerColor, textColor, font }: NavBarProps) => {
   return (
     <div
-      className={`${font ? font.className : cedarville_Cursive.className} navbar p-8 ${headerColor ? headerColor : "bg-[#f0e6da]"} ${textColor ? textColor : "text-[#4f6815]"}`}
+      className={`${font ? font.className : cedarville_Cursive.className} navbar md:p-8 sm:p-2 ${headerColor ? headerColor : "bg-[#f0e6da]"} ${textColor ? textColor : "text-[#4f6815]"}`}
     >
-      <Link href="/" className="flex-auto btn btn-ghost text-xl">
+      <Link href="/" className="flex-auto btn btn-ghost md:text-xl">
         Home
       </Link>
-      <Link href="about/" className="flex-auto btn btn-ghost text-xl">
+      <Link href="about/" className="flex-auto btn btn-ghost md:text-xl">
         About the Host
       </Link>
       <Link
         href="personal-projects/"
-        className="flex-auto btn btn-ghost text-xl"
+        className="flex-auto btn btn-ghost md:text-xl"
       >
         The Menu
       </Link>
-      <Link href="resume/" className="flex-auto btn btn-ghost text-xl">
+      <Link href="resume/" className="flex-auto btn btn-ghost md:text-xl">
         Wine List
       </Link>
-      <Link href="contact/" className="flex-auto btn btn-ghost text-xl">
+      <Link href="contact/" className="flex-auto btn btn-ghost md:text-xl">
         Guest Book
       </Link>
     </div>
